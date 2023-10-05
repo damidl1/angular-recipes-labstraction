b78ca3d853eda31ec376e610cfb8c59777c64745
labstraction <45163263+labstraction@users.noreply.github.com>
2023-10-05 09:22:46 +0200

    change select position

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DishType } from 'src/app/model/recipe';
 
 @Component({
   selector: 'app-header',
@@ -11,15 +14,30 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  selectedCategory: string = '-1';
+
+  categories = Object.entries(DishType).slice(Object.entries(DishType).length/2);
+
   toggleTheme(){
     document.body.classList.toggle('pippo-dark')
+  }
+
+  categoryChanged(){
+    // if(this.selectedCategory === '-1'){
+    //   this.recipes = this.allRecipes;
+    // } else {
+    //   const categoryNumber = parseInt(this.selectedCategory);
+    //   this.recipes = this.allRecipes.filter(recipe => recipe.category === categoryNumber)
+    // }
   }
 
 }
--- src/app/components/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes-list/recipes-list.component.ts
@@ -4,8 +4,7 @@ import { DataService } from 'src/app/services/data.service';
 import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
 import { DishType, Recipe } from 'src/app/model/recipe';
 import {MatGridListModule} from '@angular/material/grid-list';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-recipes-list',
@@ -13,9 +12,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   imports: [
     CommonModule,
     RecipeCardComponent,
-    MatGridListModule,
-    MatSelectModule,
-    MatFormFieldModule
+    MatGridListModule
   ],
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
@@ -24,9 +21,7 @@ export class RecipesListComponent implements OnInit{
 
   allRecipes: Recipe[] = [];
   recipes: Recipe[] = [];
-  selectedCategory: string = '-1';
 
-  categories = Object.entries(DishType).slice(Object.entries(DishType).length/2);
 
   constructor(private dataServ: DataService){}
 
@@ -42,13 +37,6 @@ export class RecipesListComponent implements OnInit{
     // console.log(Object.entries(DishType).slice(Object.entries(DishType).length/2))
   }
 
-  categoryChanged(){
-    if(this.selectedCategory === '-1'){
-      this.recipes = this.allRecipes;
-    } else {
-      const categoryNumber = parseInt(this.selectedCategory);
-      this.recipes = this.allRecipes.filter(recipe => recipe.category === categoryNumber)
-    }
-  }
+
 
 }
